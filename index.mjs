8bc679407a57920249f5012bd41ed35a9b8c3390
Winston Yee <winsyee@gmail.com>
2024-09-29 13:46:11 -0700

    feat: Working sending email

--- index.mjs
+++ index.mjs
@@ -1,6 +1,21 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+import FormData from 'form-data'
+import Mailgun from 'mailgun.js'
+
+const EMAIL_DOMAIN = process.env.EMAIL_DOMAIN
+const MAILGUN_API_KEY = process.env.MAILGUN_API_KEY
+const FROM_EMAIL = process.env.FROM_EMAIL
+const TO_EMAIL = process.env.TO_EMAIL.split(',')
+const SEND_EMAIL = process.env.SEND_EMAIL
+
+const mailgun = new Mailgun(FormData)
+const mg = mailgun.client({
+  username: 'api',
+  key: MAILGUN_API_KEY,
+})
+
 let puppeteer
 let chromium
 let executablePath
@@ -12,6 +27,31 @@ if (process.env.IS_LOCAL) {
   puppeteer = await import('puppeteer-core') // AWS Lambda
   chromium = await import('chrome-aws-lambda')
   executablePath = await chromium.executablePath
+}
+
+async function sendMail(content) {
+  console.log({ EMAIL_DOMAIN, MAILGUN_API_KEY, FROM_EMAIL, TO_EMAIL })
+
+  // Assuming content is a base64 string
+  const buffer = Buffer.from(content, 'base64')
+
+  try {
+    const messageData = {
+      from: process.env.FROM_EMAIL,
+      to: process.env.TO_EMAIL,
+      subject: 'Puppeteer Screenshot Attachment',
+      text: 'Please find the screenshot attached.',
+      attachment: {
+        data: buffer,
+        filename: 'screenshot.jpg', // or 'screenshot.png', depending on format
+      },
+    }
+
+    const response = await mg.messages.create(process.env.EMAIL_DOMAIN, messageData)
+    console.log('Email sent successfully:', response)
+  } catch (error) {
+    console.error('Error sending email:', error)
+  }
 }
 
 export const handler = async (event, context) => {
@@ -35,21 +75,25 @@ export const handler = async (event, context) => {
 
     await page.setViewport({
       width: 1280,
-      height: 5000, // Set a large enough height to cover more content before scrolling
+      height: 2000, // Set a large enough height to cover more content before scrolling
     })
 
     // Scroll to the bottom of the page to dynamically load all products
     // Scroll to the middle of the page
-    await page.evaluate(() => {
-      window.scrollTo(0, document.body.scrollHeight / 2)
-    })
+    // await page.evaluate(() => {
+    //   window.scrollTo(0, document.body.scrollHeight / 4)
+    // })
 
     const screenshot = await page.screenshot({
       encoding: 'base64',
-      fullpage: true,
+      fullpage: false,
       type: 'jpeg',
       quality: 50,
     })
+
+    if (process.env.SEND_EMAIL) {
+      await sendMail(screenshot)
+    }
 
     return {
       statusCode: 200,
